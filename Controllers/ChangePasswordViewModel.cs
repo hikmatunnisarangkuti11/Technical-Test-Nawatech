634ae489005ba2154e0f03c37b06f725344bc72f
hikmatunnisarangkuti11 <hikmatunnisa72@gmail.com>
2025-05-23 21:45:20 +0700

    first commit

---
+++ Controllers/ChangePasswordViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Project1.Controllers
+{
+    internal class ChangePasswordViewModel
+    {
+        public string Email { get; set; }
+        public string NewPassword { get; internal set; }
+    }
+}
