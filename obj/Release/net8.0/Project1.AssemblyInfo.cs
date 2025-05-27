3dee68307aa93177d7154eda962a25b74e01e3b4
hikmatunnisarangkuti11 <hikmatunnisa72@gmail.com>
2025-05-27 09:23:05 +0700

    clear code

--- obj/Release/net8.0/Project1.AssemblyInfo.cs
+++ obj/Release/net8.0/Project1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f567c09872b47995c36a851e29810c24f16fb78")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b742528749ef171ee499380717fbe92b009c84d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
